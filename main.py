e79f2ec05ce12834dd2ccc3ae190af4efde3cbb9
MoritzSchwerer <moritz.schwerer@gmx.de>
2022-07-21 10:00:33 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import torch
+import torch.nn as nn
+
+class SelfAttention(nn.Module):
+    def __init__(self, embed_size, num_heads):
+        super(SelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.num_heads = num_heads
+        self.head_dims = embed_size // num_heads
+        #assert(self.head_dims * num_heads == embed_size, "embed_size/num_heads is not valid")
+        self.keys = nn.Linear(self.head_dims, self.head_dims)
+        self.queries = nn.Linear(self.head_dims, self.head_dims)
+        self.values = nn.Linear(self.head_dims, self.head_dims)
+        self.fc_out = nn.Linear(embed_size, embed_size)
+
+    def forward(self, keys, queries, values, mask=None):
+        N = values.shape[0]
+        key_len, query_len, value_len = keys.shape[1], queries.shape[1], values.shape[1]
+
+        keys = keys.reshape(N, key_len, self.num_heads, self.head_dims)
+        queries = keys.reshape(N, query_len, self.num_heads, self.head_dims)
+        values = keys.reshape(N, value_len, self.num_heads, self.head_dims)
+
+        keys = self.keys(keys)
+        queries = self.queries(queries)
+        values = self.values(values)
+
+        energy = torch.einsum("nqhd,nkhd->nhqk", [queries, keys])
+
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, float("-1e20"))
+
+        attention = torch.softmax(energy / (self.embed_size ** (1/2)), dim=3)
+
+        out = torch.einsum("nhqk,nvhd->nqhd", [energy, values])
+        out = out.reshape(N, value_len, self.embed_size)
+        out = self.fc_out(out)
+        return out
+
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_size, num_heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, num_heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+        self.dropout = nn.Dropout(dropout)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, embed_size*forward_expansion),
+            nn.ReLU(),
+            nn.Linear(embed_size*forward_expansion, embed_size)
+        )
+
+    def forward(self, keys, queries, values, mask=None):
+        attention = self.attention(keys, queries, values, mask)
+        x = self.dropout(self.norm1(attention + values))
+        fwd = self.feed_forward(x)
+        out = self.dropout(self.norm2(fwd + x))
+        return out
+
+class Encoder(nn.Module):
+    def __init__(self, src_vocab_size, embed_size, num_layers,
+                 num_heads, device, forward_expansion, dropout,
+                 max_length):
+        super(Encoder, self).__init__()
+        self.word_embedding = nn.Embedding(src_vocab_size, embed_size)
+        self.pos_embedding = nn.Embedding(max_length, embed_size)
+        self.layers = nn.ModuleList(
+            [
+                TransformerBlock(embed_size, num_heads, dropout, forward_expansion)
+                for _ in range(num_layers)
+            ]
+        )
+        self.dropout = nn.Dropout(dropout)
+        self.device = device
+
+    def forward(self, x, mask=None):
+        N, seq_len = x.shape[0], x.shape[1]
+        pos = torch.arange(0, seq_len).expand(N, seq_len).to(self.device)
+        out = self.dropout(
+            self.word_embedding(x) + self.pos_embedding(pos)
+        )
+        for layer in self.layers:
+            out = layer(out, out, out, mask)
+        return out
+
+
+
+if __name__ == "__main__":
+    model = Encoder(1000, 256, 10, 8, "cpu", 4, 0.1, 50)
+    input = torch.randint(0, 1, (1000, 10))
+    output = model(input)
+    if (input.shape == output.shape):
+        print("it works")
+    else:
+        print("doesn't work")
+        print(output.shape)
+
+    embed = nn.Embedding(1000, 5)
+    print(embed(torch.IntTensor([1,100,999, 5, 6, 7, 8, 9,10])).shape)
